cb473df51145bf290e72e0cc2a6d6af40dee17bb
mtorres9095 <mtorres9095@gmail.com>
2020-06-02 14:43:40 -0400

    Add project files.

---
+++ RectangleExercise1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RectangleExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length;
+            double width;
+            string inputLength;
+            string inputWidth;
+            double areaOfRectangle;
+            // string result;
+
+            Console.WriteLine("Area of a Regtangle:");
+            Console.WriteLine("Enter the LENGTH:");
+            inputLength = Console.ReadLine();
+            length = double.Parse(inputLength);
+            Console.WriteLine("Enter the WIDTH:");
+            inputWidth = Console.ReadLine();
+            width = double.Parse(inputWidth);
+            areaOfRectangle = (length*width);
+            // result = areaOfRectangle;
+            Console.WriteLine("Give a length of "+ inputLength+" inches and width of "+inputWidth+" inches the area is: "+areaOfRectangle+" inches");
+
+
+        }
+    }
+}
